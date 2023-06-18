979f787a0687b23af673af366ea66864d1b1bb64
Tyler Underwood <twood9600@yahoo.com>
2023-06-18 12:49:24 -0400

    developed the structure for the readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -37,11 +37,59 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = '';
+
+  if(license === 'none') {
+    licenseSection = ''
+  } else {
+    licenseSection = `license : ${license}`
+  }
+
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  # ${data.title}
+
+  ## Table of Contents:
+
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributions](#contributions)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Description: 
+  -  ${data.description}
+
+  ## Installation:
+  - The following is required for the application to function correctly: 
+  - ${data.installation}
+
+  ## Usage
+  - ${data.usage}
+
+  ## License
+  - ${renderLicenseSection(data.license)} ${renderLicenseBadge(data.license)}
+
+  - ${renderLicenseLink(data.license)}
+
+  ## Contributions
+  - ${data.contributions}
+
+  ## Tests
+  - ${data.tests}
+
+  ## Questions
+  - If you have any questions, you can contact me here.
+  - Github: https://github.com/${data.github}
+  - Email: ${data.email}
 
 `;
 }
